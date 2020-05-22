56cbdb217b8a6427c5a8909859b007e912237678
NKUMBE “nkaurelien” AURELIEN <nkaurelien@gmail.com>
2020-05-22 03:36:11 +0100

    FEAT! Handle any global errors

--- src/bugsnag.service.ts
+++ src/bugsnag.service.ts
@@ -2,14 +2,38 @@ import Bugsnag from '@bugsnag/js';
 import {Injectable} from '@nestjs/common';
 
 import {BugsnagClient, BugsnagModuleOptions} from './bugsnag.interfaces';
+import {NestExpressApplication} from "@nestjs/platform-express";
 
 @Injectable()
 export class BugsnagService {
 
+    private _option;
+
     private readonly _instance: BugsnagClient;
 
     constructor(options: BugsnagModuleOptions) {
-        this._instance = Bugsnag.createClient(options);
+        this._option = options;
+        if (!this._instance) {
+            this._instance = Bugsnag.start(options);
+        }
+    }
+
+    handleAnyErrors(app: NestExpressApplication): any {
+        if (!this._instance) {
+            console.log('Bugsnag not started');
+            return;
+        }
+        let middleware = Bugsnag.getPlugin('express');
+
+        // This must be the first piece of middleware in the stack.
+        // It can only capture errors in downstream middleware
+        app.use(middleware.requestHandler);
+
+        /* all other middleware and application routes go here */
+
+        // This handles any errors that Express catches
+        app.use(middleware.errorHandler);
+
     }
 
     get instance(): BugsnagClient {
